84b4ffe03d031042e9139e9844958f8bd74c7ce1
Strapchay <ayanelaw@outlook.com>
2024-10-04 09:01:47 -0400

    added default auth for login

--- src/js/loginViews/loginForm.js
+++ src/js/loginViews/loginForm.js
@@ -2,51 +2,51 @@ import { BaseForm } from "./baseForm.js";
 import { API } from "../api.js";
 
 export class LoginForm extends BaseForm {
-    constructor() {
-        super()
-    }
+  constructor() {
+    super();
+  }
 
-    _handleEvents(ev) {
-        if (ev.type === "submit") this._handleSubmit(ev)
-    }
+  _handleEvents(ev) {
+    if (ev.type === "submit") this._handleSubmit(ev);
+  }
 
-    _handleSubmit(ev) {
-        const payload = this.createPayload(ev)
-        if (!payload) return;
-        //entrypoint to api
-        const queryObj = {
-            endpoint: API.APIEnum.USER.TOKEN,
-            sec: null,
-            actionType: "login",
-            queryData: payload,
-            callBack: this.loginWithToken.bind(this),
-            spinner: true,
-            alert: true,
-            type: null
-        }
-        API.queryAPI(queryObj)
-    }
+  _handleSubmit(ev) {
+    const payload = this.createPayload(ev);
+    if (!payload) return;
+    //entrypoint to api
+    const queryObj = {
+      endpoint: API.APIEnum.USER.TOKEN,
+      sec: null,
+      actionType: "login",
+      queryData: payload,
+      callBack: this.loginWithToken.bind(this),
+      spinner: true,
+      alert: true,
+      type: null,
+    };
+    API.queryAPI(queryObj);
+  }
 
-    loginWithToken(token, success) { //NOTE: do not remove success param
-        if (token)
-            //offload to controller
-            this.controlHandler(token)
+  loginWithToken(token, success) {
+    //NOTE: do not remove success param
+    if (token)
+      //offload to controller
+      this.controlHandler(token);
+  }
 
-    }
-
-    _generateMarkup() {
-        return `
+  _generateMarkup() {
+    return `
             <form action="" id="login-form" class="form-class">
                 <div class="email-box form-box">
-                    <input type="email" name="email" placeholder="email" required>
+                    <input type="email" name="email" placeholder="email" value="testuser123@email.com" required>
                 </div>
                 <div class="password-box form-box">
-                    <input type="password" name="password" placeholder="password" required>
+                    <input type="password" name="password" placeholder="password" value="Testuser123" required>
                 </div>
                 <button class="btn-login btn-submit">Submit</button>
 
             </form>
 
-        `
-    }
+        `;
+  }
 }
